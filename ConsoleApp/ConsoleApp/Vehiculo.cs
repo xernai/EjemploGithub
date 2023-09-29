57d489949048075a2f480968e28a06be7c6513ca
Xabier <montcouronne@hotmail.com>
2023-09-29 11:49:47 -0600

    Adding Vehiculo class

---
+++ ConsoleApp/ConsoleApp/Vehiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Vehiculo
+    {
+        public decimal VelocidadMaxima { get; set; }
+
+        public Vehiculo(decimal velocidadMaxima)
+        {
+            VelocidadMaxima = velocidadMaxima;
+        }
+    }
+}
