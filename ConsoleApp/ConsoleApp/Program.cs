3272a19d7c2ede5aeaa763caa5d9f92e9bdc95db
Xabier <montcouronne@hotmail.com>
2023-09-29 15:56:40 -0600

    Add Cv en el vehiculo

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -9,6 +9,10 @@ namespace ConsoleApp
             Console.WriteLine("Introudce tu nombre:");
             string nombre = Console.ReadLine();
             Console.WriteLine($"El nombre es {nombre}");
+            
+            var coche = new Vehiculo(200, 5, 90);
+            Console.WriteLine($"El coche tiene {coche.Cv}");
+            Console.ReadKey();
         }
     }
 }
